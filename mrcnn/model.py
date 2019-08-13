b98dd80842c95487ad842c216b8868cac5c9a073
pnuehren <paul.nuehrenberg@uni.kn>
2019-08-13 11:29:19 +0200

    update model

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -30,10 +30,10 @@ from distutils.version import LooseVersion
 assert LooseVersion(tf.__version__) >= LooseVersion("1.3")
 assert LooseVersion(keras.__version__) >= LooseVersion('2.0.8')
 
-# Required for cudnn after changing resolution
-config = tf.ConfigProto()
-config.gpu_options.allow_growth = True
-session = tf.Session(config=config)
+# # Required for cudnn after changing resolution
+# config = tf.ConfigProto()
+# config.gpu_options.allow_growth = True
+# session = tf.Session(config=config)
 
 ############################################################
 #  Utility Functions
